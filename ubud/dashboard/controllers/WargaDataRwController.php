86d646f895986761cc5ad94c86e30368b6149186
Piter Novian <piter@lipat.co.id>
2016-02-28 10:18:55 +0700

    update

--- ubud/dashboard/controllers/WargaDataRwController.php
+++ ubud/dashboard/controllers/WargaDataRwController.php
@@ -32,6 +32,33 @@ class WargaDataRwController extends Controller
         ];
     }
 
+	/**
+     * Before Action Index
+	 * @author ptrnov  <piter@lukison.com>
+	 * @since 1.1
+     */
+	public function beforeAction(){
+			if (Yii::$app->user->isGuest)  {
+				 Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+			}
+            // Check only when the user is logged in
+            if (!Yii::$app->user->isGuest)  {
+               if (Yii::$app->session['userSessionTimeout']< time() ) {
+                   // timeout
+                   Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+               } else {
+                   //Yii::$app->user->setState('userSessionTimeout', time() + Yii::app()->params['sessionTimeoutSeconds']) ;
+				   Yii::$app->session->set('userSessionTimeout', time() + Yii::$app->params['sessionTimeoutSeconds']);
+                   return true;
+               }
+            } else {
+                return true;
+            }
+    }
+
+	
     /**
      * Lists all Warga_data models.
      * @return mixed
