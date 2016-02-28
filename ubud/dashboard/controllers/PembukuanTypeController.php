440dc123067f5dac79a701c0ccd4eba20a7360d5
Piter Novian <piter@lipat.co.id>
2016-02-28 10:23:26 +0700

    update

--- ubud/dashboard/controllers/PembukuanTypeController.php
+++ ubud/dashboard/controllers/PembukuanTypeController.php
@@ -26,6 +26,32 @@ class PembukuanTypeController extends Controller
         ];
     }
 
+	/**
+     * Before Action Index
+	 * @author ptrnov  <piter@lukison.com>
+	 * @since 1.1
+     */
+	public function beforeAction(){
+			if (Yii::$app->user->isGuest)  {
+				 Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+			}
+            // Check only when the user is logged in
+            if (!Yii::$app->user->isGuest)  {
+               if (Yii::$app->session['userSessionTimeout']< time() ) {
+                   // timeout
+                   Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+               } else {
+                   //Yii::$app->user->setState('userSessionTimeout', time() + Yii::app()->params['sessionTimeoutSeconds']) ;
+				   Yii::$app->session->set('userSessionTimeout', time() + Yii::$app->params['sessionTimeoutSeconds']);
+                   return true;
+               }
+            } else {
+                return true;
+            }
+    }
+	
     /**
      * Lists all Pembukuan_type models.
      * @return mixed
